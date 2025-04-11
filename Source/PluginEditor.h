ac54c46dd61fc870d8fa86290f8fbfaa58723c38
Zachary Pennington <mixedbyziptye@gmail.com>
2025-04-11 15:02:45 -0700

    Adds LEDs for Stereo Correlation meter, but needs refactoring

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -54,6 +54,48 @@ private:
     juce::Rectangle<int> widthUp {609, 143, 17, 17}; // Width Up
     juce::Rectangle<int> widthDown {609, 211, 17, 17}; // Width Down
     
+    //==========================================================================
+    // STEREO CORRLEATION METER LEDS
+    // TODO: NEED TO REFACTOR THIS CODE... THIS IS JUST BAD LOL :)
+    
+    juce::Rectangle<int> led1L {478, 255, 8, 8};
+    juce::Rectangle<int> led1R {478, 268, 8, 8};
+    
+    juce::Rectangle<int> led2L {493, 255, 8, 8};
+    juce::Rectangle<int> led2R {493, 268, 8, 8};
+    
+    juce::Rectangle<int> led3L {508, 255, 8, 8};
+    juce::Rectangle<int> led3R {508, 268, 8, 8};
+    
+    juce::Rectangle<int> led4L {523, 255, 8, 8};
+    juce::Rectangle<int> led4R {523, 268, 8, 8};
+    
+    juce::Rectangle<int> led5L {538, 255, 8, 8};
+    juce::Rectangle<int> led5R {538, 268, 8, 8};
+    
+    juce::Rectangle<int> led6L {553, 255, 8, 8};
+    juce::Rectangle<int> led6R {553, 268, 8, 8};
+    
+    juce::Rectangle<int> led7L {576, 255, 8, 8};
+    juce::Rectangle<int> led7R {576, 268, 8, 8};
+    
+    juce::Rectangle<int> led8L {591, 255, 8, 8};
+    juce::Rectangle<int> led8R {591, 268, 8, 8};
+    
+    juce::Rectangle<int> led9L {606, 255, 8, 8};
+    juce::Rectangle<int> led9R {606, 268, 8, 8};
+    
+    juce::Rectangle<int> led10L {621, 255, 8, 8};
+    juce::Rectangle<int> led10R {621, 268, 8, 8};
+    
+    juce::Rectangle<int> led11L {636, 255, 8, 8};
+    juce::Rectangle<int> led11R {636, 268, 8, 8};
+    
+    juce::Rectangle<int> led12L {651, 255, 8, 8};
+    juce::Rectangle<int> led12R {651, 268, 8, 8};
+    
+    
+    //==========================================================================
     // Listener callback
     void parameterChanged(const juce::String& parameterID, float newValue) override;
 
